ada41871bf8e47328852d521bd3ea759a9c05193
Ruby <LIU10032@163.com>
2022-02-27 09:54:03 +0800

    feat: setup vue di system and render the ui

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import { createApp } from "vue";
+import { createApp, h, provide } from "vue";
 import { createPinia } from "pinia";
 import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+import { DefaultApolloClient } from "@vue/apollo-composable";
 
 import App from "./App.vue";
 import router from "./router";
@@ -10,23 +11,31 @@ const defaultClient = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
-const query = gql`
-  query {
-    characters {
-      results {
-        name
-      }
-    }
-  }
-`;
+// const query = gql`
+//   query {
+//     characters {
+//       results {
+//         name
+//       }
+//     }
+//   }
+// `;
 
-defaultClient
-  .query({
-    query,
-  })
-  .then((res) => console.log(res));
+// defaultClient
+//   .query({
+//     query,
+//   })
+//   .then((res) => res.data.characters.results)
+//   .then((res) => console.log(res));
 
-const app = createApp(App);
+const app = createApp({
+  setup() {
+    provide(DefaultApolloClient, defaultClient);
+  },
+  render() {
+    return h(App);
+  },
+});
 
 app.use(createPinia());
 app.use(router);
